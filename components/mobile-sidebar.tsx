3bf2a689726a954b572e034fd330e97cafd7bb38
Joshua Juanzo <juanzojosh@gmail.com>
2023-08-22 12:28:23 +0800

    add prisma setup and planet scale

---
+++ components/mobile-sidebar.tsx
@@ -0,0 +1,19 @@
+import { Menu } from 'lucide-react';
+
+import Sidebar from '@/components/sidebar';
+import { Sheet, SheetContent, SheetTrigger } from '@/components/ui/sheet';
+
+const MobileSidebar = () => {
+  return (
+    <Sheet>
+      <SheetTrigger className="md:hidden pr-4">
+        <Menu />
+      </SheetTrigger>
+      <SheetContent side="left" className="p-0 bg-secondary pt-10 w-32">
+        <Sidebar />
+      </SheetContent>
+    </Sheet>
+  );
+};
+
+export default MobileSidebar;
