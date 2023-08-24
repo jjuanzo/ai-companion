3ede08956fe2462b71596156d5f32e98d610cb5c
Joshua Juanzo <juanzojosh@gmail.com>
2023-08-24 11:54:29 +0800

    fix typescript error

--- components/mobile-sidebar.tsx
+++ components/mobile-sidebar.tsx
@@ -3,14 +3,18 @@ import { Menu } from 'lucide-react';
 import Sidebar from '@/components/sidebar';
 import { Sheet, SheetContent, SheetTrigger } from '@/components/ui/sheet';
 
-const MobileSidebar = () => {
+interface MobileSidebarProps {
+  isPro: boolean;
+}
+
+const MobileSidebar = ({ isPro }: MobileSidebarProps) => {
   return (
     <Sheet>
       <SheetTrigger className="md:hidden pr-4">
         <Menu />
       </SheetTrigger>
       <SheetContent side="left" className="p-0 bg-secondary pt-10 w-32">
-        <Sidebar />
+        <Sidebar isPro={isPro} />
       </SheetContent>
     </Sheet>
   );
